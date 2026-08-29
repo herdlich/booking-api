97330c3ae59efc49e42d8abcb6a47cdf2f587e4d
herdlich <herdlich@gmail.com>
2026-08-29 12:24:47 +0300

    style(routers): improve endpoint summaries

--- src/app/routers/bookings.py
+++ src/app/routers/bookings.py
@@ -10,19 +10,19 @@ from src.app.database import get_session
 router = APIRouter(prefix="/bookings", tags=["Bookings"])
 
 
-@router.post("", response_model=BookingResponse, summary="Create booking")
+@router.post("", response_model=BookingResponse, summary="Create Booking")
 def create_booking_endpoint(data: BookingCreate, session: Annotated[Session, Depends(get_session)], current_user: Annotated[User, Depends(get_current_user)]):
     booking = create_booking(session, data, current_user.user_id)
     return booking
 
 
-@router.get("/my", response_model=list[BookingResponse], summary="Show my bookings")
+@router.get("/my", response_model=list[BookingResponse], summary="Show My Bookings")
 def get_my_bookings_endpoint(session: Annotated[Session, Depends(get_session)], current_user: Annotated[User, Depends(get_current_user)]):
     bookings = get_my_bookings(session, current_user.user_id)
     return bookings
 
 
-@router.delete("/{booking_id}", summary="Delete booking")
+@router.delete("/{booking_id}", summary="Delete Booking")
 def delete_booking_endpoint(booking_id: int, session: Annotated[Session, Depends(get_session)], current_user: Annotated[User, Depends(get_current_user)]):
     delete_booking(session = session, booking_id = booking_id, user_id = current_user.user_id)
     return {"status": "success", "message": "booking deleted"}
--- src/app/routers/rooms.py
+++ src/app/routers/rooms.py
@@ -3,14 +3,13 @@ from sqlalchemy.orm import Session
 from typing import Annotated
 
 from src.app.schemas import RoomCreate, RoomResponse
-from src.app.models import Room
 from src.app.service import create_room, get_all_rooms, delete_room
 from src.app.database import get_session
 
 router = APIRouter(prefix="/rooms", tags=["Rooms"])
 
 
-@router.post("", response_model=RoomResponse, summary="Room create")
+@router.post("", response_model=RoomResponse, summary="Create Room")
 def create_room_endpoint(data: RoomCreate, session: Annotated[Session, Depends(get_session)]):
     room = create_room(session, data)
     return room
