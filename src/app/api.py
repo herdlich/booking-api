716599c36f7fbecf70cfd513b87cab34250389f3
herdlich <herdlich@gmail.com>
2026-09-12 21:04:52 +0400

    feat(api): add global unauthorized handler

--- src/app/api.py
+++ src/app/api.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from src.app.routers import users, bookings, rooms
+from src.app.exceptions import UnauthorizedError
 
 app = FastAPI(
     title="Booking API",
     description="Room booking API Project",
     version="beta"
 )
+
+@app.exception_handler(UnauthorizedError)
+def unauthorized_handler(
+        _request: Request,
+        _exc: UnauthorizedError,
+):
+    return JSONResponse(
+        status_code=401,
+        content={"detail": "Unauthorized"},
+        headers={"WWW-Authenticate": "Bearer"},
+    )
 
 app.include_router(users.router)
 app.include_router(bookings.router)
